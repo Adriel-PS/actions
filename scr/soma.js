1ca6ffdff441367caf70ff07beaac505fe8a99c5
adriel siqueira <adrielsiqueira@plusoft.com>
2025-11-17 12:01:37 -0300

    testes unitarios

--- scr/soma.js
+++ scr/soma.js
@@ -2,4 +2,36 @@ function soma(a,b){
     return a + b;
 }
 
-module.exports = soma;  
+function divisao(a,b){
+    return a / b;
+}
+
+function multiplicacao(a,b){
+    return a * b;
+}   
+
+function subtracao(a,b){
+    return a - b;
+}   
+
+function potencia(a,b){
+    return a ** b;
+}       
+
+function raizQuadrada(a){
+    const resultado = a ** 0.5;
+    if (Number.isInteger(resultado)){
+        return resultado;
+    }
+    else {
+        return resultado.toFixed(2);
+    }
+}
+
+module.exports = soma;
+module.exports = divisao;
+module.exports = multiplicacao;
+module.exports = subtracao;
+module.exports = potencia;
+module.exports = raizQuadrada;
+    
--- tests/soma.test.js
+++ tests/soma.test.js
@@ -1,5 +1,11 @@
 const soma = require('../scr/soma.js');
+const potencia = require('../scr/soma.js');
+
 
 test('soma 1 + 3 igual a 4', () => {
     expect(soma(1,3)).toBe(4);
+});
+
+test ('3 elevado a 3 igual a 27', () => {
+    expect(potencia(3,3)).toBe(27);
 });
